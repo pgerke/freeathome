06a23e94c8b819abf6aa40bb319f536ff39e73f9
Philip Gerke <me@philipgerke.com>
2025-06-01 18:31:20 +0200

    test(integration): finish integration tests
    Ref: #16
    Signed-off-by: Philip Gerke <me@philipgerke.com>

--- integration/monitor_test.go
+++ integration/monitor_test.go
@@ -3,9 +3,14 @@
 package integration
 
 import (
+	"net"
+	"net/http"
 	"os"
 	"os/exec"
 	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 const bin = "./../monitor-integration.test"
@@ -31,5 +36,87 @@ func TestMonitor_MissingEnvs(t *testing.T) {
 
 	if exitCode != 1 {
 		t.Errorf("expected exit code 1, got %d", exitCode)
+	}
+}
+
+func TestMonitor_SuccessfulRun(t *testing.T) {
+	// Set up the test server
+	addr, shutdown := startTestWebSocketServer(t)
+	defer shutdown()
+
+	// Run the monitor
+	run := exec.Command(bin, "-test.run=TestMonitor_Main")
+	run.Env = append(os.Environ(),
+		"RUN_MAIN=1",
+		"GOCOVERDIR="+coverageDirectory,
+		"SYSAP_HOST="+addr,
+		"SYSAP_USER_ID=admin",
+		"SYSAP_PASSWORD=password",
+	)
+
+	if err := run.Start(); err != nil {
+		t.Fatalf("could not start monitor: %v", err)
+	}
+
+	time.Sleep(2500 * time.Millisecond) // Allow some time for the monitor to connect
+
+	// Send an interrupt signal to the monitor process
+	if err := run.Process.Signal(os.Interrupt); err != nil {
+		t.Fatalf("could not send interrupt signal: %v", err)
+	}
+
+	// Wait for the monitor to finish
+	err := run.Wait()
+	var exitCode int
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			exitCode = exitErr.ExitCode()
+		} else {
+			t.Fatalf("could not get exit code: %v", err)
+		}
+	}
+
+	if exitCode != 0 {
+		t.Errorf("expected exit code 0, got %d", exitCode)
+	}
+}
+
+func startTestWebSocketServer(t *testing.T) (addr string, shutdown func()) {
+	upgrader := websocket.Upgrader{}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			t.Logf("Failed to upgrade WebSocket: %v", err)
+			return
+		}
+		defer conn.Close()
+
+		// Echo everything
+		for {
+			mt, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+			_ = conn.WriteMessage(mt, msg)
+		}
+	})
+
+	// Listen on a random port
+	ln, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("Failed to listen: %v", err)
+	}
+
+	server := &http.Server{
+		Handler: mux,
+	}
+
+	go server.Serve(ln)
+
+	// Return address and shutdown function
+	return ln.Addr().String(), func() {
+		server.Close()
 	}
 }
