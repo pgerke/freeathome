8b3a56bcbfba61799338c465d2364a1ec63f29cd
Philip Gerke <me@philipgerke.com>
2025-06-10 06:44:49 +0200

    test(unit): create unit tests
    Ref: #7
    Signed-off-by: Philip Gerke <me@philipgerke.com>

--- pkg/freeathome/system-access-point_create-virtual-device_test.go
+++ pkg/freeathome/system-access-point_create-virtual-device_test.go
@@ -10,21 +10,36 @@ import (
 	"github.com/pgerke/freeathome/pkg/models"
 )
 
-// TestSystemAccessPoint_GetConfiguration tests the GetConfiguration method of SystemAccessPoint.
-func TestSystemAccessPoint_GetConfiguration(t *testing.T) {
+func newVirtualDevice(t *testing.T) *models.VirtualDevice {
+	t.Helper()
+	properties := &models.VirtualDeviceProperties{
+		TTL:          nil,
+		DisplayName:  nil,
+		Flavor:       nil,
+		Capabilities: &[]uint{1, 2, 3},
+	}
+	return &models.VirtualDevice{
+		Type:       models.BinarySensor,
+		Properties: *properties,
+	}
+}
+
+func TestSystemAccessPoint_CreateVirtualDevice(t *testing.T) {
 	sysAp, buf, _ := setup(t, true)
 	response := &http.Response{
 		StatusCode: http.StatusOK,
-		Body:       loadTestResponseBody(t, "configuration.json"),
+		Body:       loadTestResponseBody(t, "virtualdevice.json"),
 		Header:     make(http.Header),
 	}
+
 	roundtripper := &MockRoundTripper{
 		Response: response,
 		Err:      nil,
 	}
 	sysAp.client.SetTransport(roundtripper)
 
-	result, err := sysAp.GetConfiguration()
+	device := newVirtualDevice(t)
+	result, err := sysAp.CreateVirtualDevice("6000D2CB27B2", device)
 
 	// Check for errors
 	if err != nil {
@@ -38,10 +53,10 @@ func TestSystemAccessPoint_GetConfiguration(t *testing.T) {
 	}
 
 	// Check if the request method and URL are correct
-	if roundtripper.Request.Method != http.MethodGet {
-		t.Errorf("Expected GET request, got %s", roundtripper.Request.Method)
+	if roundtripper.Request.Method != http.MethodPut {
+		t.Errorf("Expected PUT request, got %s", roundtripper.Request.Method)
 	}
-	expectedUrl := "https://localhost/fhapi/v1/api/rest/configuration"
+	expectedUrl := "https://localhost/fhapi/v1/api/rest/virtualdevice/00000000-0000-0000-0000-000000000000/6000D2CB27B2"
 	if roundtripper.Request.URL.String() != expectedUrl {
 		t.Errorf("Expected URL '%s', got '%s'", expectedUrl, roundtripper.Request.URL.String())
 	}
@@ -51,15 +66,17 @@ func TestSystemAccessPoint_GetConfiguration(t *testing.T) {
 		t.Error("Expected non-nil result")
 	}
 	if len(*result) != 1 {
-		t.Errorf("Expected the configuration to contain one system access point, got %d", len(*result))
+		t.Errorf("Expected the response to contain one virtual device response, got %d", len(*result))
 	}
-	if len((*result)[models.EmptyUUID].Devices) != 76 {
-		t.Errorf("Expected 76 devices, got %d", len((*result)[models.EmptyUUID].Devices))
+	if len((*result)[models.EmptyUUID].Devices) != 1 {
+		t.Errorf("Expected 1 created virtual device, got %d", len((*result)[models.EmptyUUID].Devices))
+	}
+	if (*result)[models.EmptyUUID].Devices["abcd12345"].Serial != "6000D2CB27B2" {
+		t.Errorf("Expected created virtual device serial to be '6000D2CB27B2', got '%s'", (*result)[models.EmptyUUID].Devices["a1b2c3"].Serial)
 	}
 }
 
-// TestSystemAccessPoint_GetConfigurationCallError tests the GetConfiguration method of SystemAccessPoint
-func TestSystemAccessPoint_GetConfigurationCallError(t *testing.T) {
+func TestSystemAccessPoint_CreateVirtualDeviceCallError(t *testing.T) {
 	sysAp, buf, _ := setup(t, true)
 	error := errors.New("Test Error")
 	roundtripper := &MockRoundTripper{
@@ -68,14 +85,58 @@ func TestSystemAccessPoint_GetConfigurationCallError(t *testing.T) {
 	}
 	sysAp.client.SetTransport(roundtripper)
 
-	result, err := sysAp.GetConfiguration()
+	device := newVirtualDevice(t)
+	result, err := sysAp.CreateVirtualDevice("6000D2CB27B2", device)
 
 	// Check if the log output contains the expected error message
 	logOutput := buf.String()
-	if !strings.Contains(logOutput, "msg=\"failed to get configuration\"") ||
-		!strings.Contains(logOutput, "error=\"Get \\\"https://localhost/fhapi/v1/api/rest/configuration\\\": Test Error\"") {
+	if !strings.Contains(logOutput, "msg=\"failed to create virtual device\"") ||
+		!strings.Contains(logOutput, "error=\"Put \\\"https://localhost/fhapi/v1/api/rest/virtualdevice/00000000-0000-0000-0000-000000000000/6000D2CB27B2\\\": Test Error") {
 		t.Errorf("Unexpected log output, got: %s", logOutput)
 	}
+
+	// Check if result is nil and error is not nil
+	if result != nil {
+		t.Errorf("Expected result to be nil, got %v", result)
+	}
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
+
+	// Check if the error message is correct
+	expected := "Put \"https://localhost/fhapi/v1/api/rest/virtualdevice/00000000-0000-0000-0000-000000000000/6000D2CB27B2\": Test Error"
+	if err.Error() != expected {
+		t.Errorf("Expected error '%s', got '%v'", expected, err)
+	}
+}
+
+func TestSystemAccessPoint_CreateVirtualDeviceErrorResponse(t *testing.T) {
+	sysAp, buf, _ := setup(t, true)
+	response := &http.Response{
+		StatusCode: http.StatusInternalServerError,
+		Status:     "Internal Server Error",
+		Body:       io.NopCloser(strings.NewReader("Internal Server Error")),
+		Header:     make(http.Header),
+	}
+
+	roundtripper := &MockRoundTripper{
+		Response: response,
+		Err:      nil,
+	}
+	sysAp.client.SetTransport(roundtripper)
+
+	device := newVirtualDevice(t)
+	result, err := sysAp.CreateVirtualDevice("6000D2CB27B2", device)
+
+	// Check if the log output contains the expected error message
+	logOutput := buf.String()
+	if !strings.Contains(logOutput, "msg=\"failed to create virtual device\"") ||
+		!strings.Contains(logOutput, "level=ERROR") ||
+		!strings.Contains(logOutput, "status=\"Internal Server Error\"") ||
+		!strings.Contains(logOutput, "body=\"Internal Server Error\"") {
+		t.Errorf("Unexpected log output, got: %s", logOutput)
+	}
+
 	// Check if result is nil and error is not nil
 	if result != nil {
 		t.Error("Expected nil result")
@@ -84,20 +145,27 @@ func TestSystemAccessPoint_GetConfigurationCallError(t *testing.T) {
 		t.Error("Expected error, got nil")
 	}
 
+	// Check if the request method and URL are correct
+	if roundtripper.Request.Method != http.MethodPut {
+		t.Errorf("Expected PUT request, got %s", roundtripper.Request.Method)
+	}
+	expectedUrl := "https://localhost/fhapi/v1/api/rest/virtualdevice/00000000-0000-0000-0000-000000000000/6000D2CB27B2"
+	if roundtripper.Request.URL.String() != expectedUrl {
+		t.Errorf("Expected URL '%s', got '%s'", expectedUrl, roundtripper.Request.URL.String())
+	}
+
 	// Check if the error message is correct
-	expected := "Get \"https://localhost/fhapi/v1/api/rest/configuration\": Test Error"
+	expected := "failed to create virtual device: Internal Server Error"
 	if err.Error() != expected {
 		t.Errorf("Expected error '%s', got '%v'", expected, err)
 	}
 }
 
-// TestSystemAccessPoint_GetConfigurationErrorResponse tests the GetConfiguration method of SystemAccessPoint
-func TestSystemAccessPoint_GetConfigurationErrorResponse(t *testing.T) {
+func TestSystemAccessPoint_CreateVirtualDeviceUnmarshalError(t *testing.T) {
 	sysAp, buf, _ := setup(t, true)
 	response := &http.Response{
-		StatusCode: http.StatusInternalServerError,
-		Status:     "Internal Server Error",
-		Body:       io.NopCloser(strings.NewReader("Internal Server Error")),
+		StatusCode: http.StatusOK,
+		Body:       io.NopCloser(strings.NewReader(`{"00000000-0000-0000-0000-000000000000":{"devices":{"abcd12345":{"serial": 123}}}}`)),
 		Header:     make(http.Header),
 	}
 	roundtripper := &MockRoundTripper{
@@ -106,75 +174,26 @@ func TestSystemAccessPoint_GetConfigurationErrorResponse(t *testing.T) {
 	}
 	sysAp.client.SetTransport(roundtripper)
 
-	result, err := sysAp.GetConfiguration()
+	device := newVirtualDevice(t)
+	result, err := sysAp.CreateVirtualDevice("6000D2CB27B2", device)
 
 	// Check if the log output contains the expected error message
 	logOutput := buf.String()
-	if !strings.Contains(logOutput, "msg=\"failed to get configuration\"") ||
-		!strings.Contains(logOutput, "level=ERROR") ||
-		!strings.Contains(logOutput, "status=\"Internal Server Error\"") ||
-		!strings.Contains(logOutput, "body=\"Internal Server Error\"") {
-		t.Errorf("Unexpected log output, got: %s", logOutput)
-	}
-
-	// Check if result is nil and error is not nil
-	if err == nil {
-		t.Fatal("Expected error, got nil")
-	}
-	if result != nil {
-		t.Error("Expected nil result")
-	}
-
-	// Check if the request method and URL are correct
-	if roundtripper.Request.Method != http.MethodGet {
-		t.Errorf("Expected GET request, got %s", response.Request.Method)
-	}
-	expectedUrl := "https://localhost/fhapi/v1/api/rest/configuration"
-	if roundtripper.Request.URL.String() != expectedUrl {
-		t.Errorf("Expected URL '%s', got '%s'", expectedUrl, roundtripper.Request.URL.String())
-	}
-
-	// Check if the error message is correct
-	expected := "failed to get configuration: Internal Server Error"
-	if err.Error() != expected {
-		t.Errorf("Expected error '%s', got '%v'", expected, err)
-	}
-}
-
-// TestSystemAccessPoint_GetConfigurationUnmarshalError tests the GetConfiguration method of SystemAccessPoint
-func TestSystemAccessPoint_GetConfigurationUnmarshalError(t *testing.T) {
-	sysAp, buf, _ := setup(t, true)
-	response := &http.Response{
-		StatusCode: http.StatusOK,
-		// This is intentionally malformed JSON to trigger the unmarshal error
-		Body:   io.NopCloser(strings.NewReader("{\"devices\": [{\"id\": \"device1\"}, {\"id\": \"device2\"}]}")),
-		Header: make(http.Header),
-	}
-	roundtripper := &MockRoundTripper{
-		Response: response,
-		Err:      nil,
-	}
-	sysAp.client.SetTransport(roundtripper)
-
-	result, err := sysAp.GetConfiguration()
-
-	// Check if the log output contains the expected error message
-	logOutput := buf.String()
-	if !strings.Contains(logOutput, "msg=\"failed to parse configuration\"") ||
+	if !strings.Contains(logOutput, "msg=\"failed to parse virtual device response\"") ||
 		!strings.Contains(logOutput, "level=ERROR") {
 		t.Errorf("Unexpected log output, got: %s", logOutput)
 	}
 
 	// Check if result is nil and error is not nil
-	if err == nil {
-		t.Fatal("Expected error, got nil")
-	}
 	if result != nil {
 		t.Error("Expected nil result")
 	}
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
 
 	// Check if the error message is correct
-	expected := "json: cannot unmarshal array into Go value of type models.SysAP"
+	expected := "json: cannot unmarshal number into Go struct field CreatedVirtualDevice.devices.serial of type string"
 	if err.Error() != expected {
 		t.Errorf("Expected error '%s', got '%v'", expected, err)
 	}
